ca79863d323fe227771aa563c0a9fa8ecd2ce934
Vadue24 <110918809+Vadue24@users.noreply.github.com>
2026-04-15 23:31:02 +0500

    Add files via upload

---
+++ base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def calculator(request):
+    return render(request, 'main/calculator.html')
